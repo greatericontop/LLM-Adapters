371d52131c780cf2044ce893efb0ee0f03261b03
greatericontop <greateric@protonmail.com>
2024-09-15 10:58:21 -0400

    llama 3 tokening

--- prompt.py
+++ prompt.py
@@ -9,27 +9,17 @@ _instr = 'Answer the question below. Make sure to explain your reasoning. An exa
 # math10k version
 #_ex_answer = 'Alice buys 3 pizzas. Each one costs $10, so the total cost of pizzas is 3 * 10 = 30. Alice buys 2 donuts. Each one costs $2, so the total cost of donuts is 2 * 2 = 4. The total cost is 30 + 4 = 34. The answer is 34.'
 
-example_block = \
-f'''### Example Question
-
-Tom has only been getting 6 hours of sleep a day.  He increases that by 1/3.  How many hours of sleep does he get per night?
-
-### Example Answer
-
-To increase his sleep by 1/3, we need to find 1/3 of his current sleep time and add it to his current sleep time.
+_examples = [
+    'Tom has only been getting 6 hours of sleep a day.  He increases that by 1/3.  How many hours of sleep does he get per night?',
+    '''To increase his sleep by 1/3, we need to find 1/3 of his current sleep time and add it to his current sleep time.
 1/3 of 6 hours is (1/3)*6 = 2 hours.
 Adding 2 hours to his current sleep time of 6 hours gives us:
 6 + 2 = 8 hours.
 Therefore, Tom now gets 8 hours of sleep per night.
-The answer in Arabic numerals is 8.
+The answer in Arabic numerals is 8.''',
 
-### Example Question
-
-Jimmy has a collection of 5 action figures. Each figure is worth $15, except for one which is worth $20. He decided to sell his collection. To do it fast he decided to sell each of them for $5 less than their value. How much will Jimmy earn if he sells all the figures?
-
-### Example Answer
-
-Step 1: Calculate the total value of the collection
+    'Jimmy has a collection of 5 action figures. Each figure is worth $15, except for one which is worth $20. He decided to sell his collection. To do it fast he decided to sell each of them for $5 less than their value. How much will Jimmy earn if he sells all the figures?',
+    '''Step 1: Calculate the total value of the collection
 - 4 action figures are worth $15 each: 4 x $15 = $60
 - 1 action figure is worth $20
 - Total value = $60 + $20 = $80
@@ -42,44 +32,74 @@ Step 3: Calculate the total earning from selling the collection
 - He will earn $10 for 4 of them: 4 x $10 = $40
 - He will earn $15 for 1 of them
 - Total earning = $40 + $15 = $55
-Therefore, Jimmy will earn $55 if he sells all the figures. The answer is 55.
+Therefore, Jimmy will earn $55 if he sells all the figures. The answer is 55.''',
 
-### Example Question
-
-There were 28 students inside a bus before stopping at an intersection. After stopping at an intersection, there were 58 students on the bus. What's 40% of the number of students who entered the bus at the intermediate stop?
-
-### Example Answer
-
-The number of students who entered the bus at the intermediate stop is:
+    'There were 28 students inside a bus before stopping at an intersection. After stopping at an intersection, there were 58 students on the bus. What\'s 40% of the number of students who entered the bus at the intermediate stop?',
+    '''The number of students who entered the bus at the intermediate stop is:
 58 - 28 = 30
 To find 40% of 30, we can multiply 30 by 0.4:
 30 x 0.4 = 12
-Therefore, 40% of the number of students who entered the bus at the intermediate stop is 12.
-'''
+Therefore, 40% of the number of students who entered the bus at the intermediate stop is 12.'''
+]
 
 
 def get_eval_prompt(instruction: str) -> str:
-    return (
-        f'{_instr}\n'
-        f'\n'
-        f'{example_block}\n'
-        f'\n'
-        f'### Question:\n'
-        f'{instruction}\n'
-        f'\n'
-        f'### Response:\n'
-    )
+    return f'''
+<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+{_instr}
+<|eot_id|>
+<|start_header_id|>user<|end_header_id|>
+{_examples[0]}
+<|eot_id|>
+<|start_header_id|>assistant<|end_header_id|>
+{_examples[1]}
+<|eot_id|>
+<|start_header_id|>user<|end_header_id|>
+{_examples[2]}
+<|eot_id|>
+<|start_header_id|>assistant<|end_header_id|>
+{_examples[3]}
+<|eot_id|>
+<|start_header_id|>user<|end_header_id|>
+{_examples[4]}
+<|eot_id|>
+<|start_header_id|>assistant<|end_header_id|>
+{_examples[5]}
+<|eot_id|>
+<|start_header_id|>user<|end_header_id|>
+{instruction}
+<|eot_id|>
+<|start_header_id|>assistant<|end_header_id|>
+'''
 
 
 def get_finetune_prompt(instruction: str, output: str) -> str:
-    return (
-        f'{_instr}\n'
-        f'\n'
-        f'{example_block}\n'
-        f'\n'
-        f'### Question:\n'
-        f'{instruction}\n'
-        f'\n'
-        f'### Response:\n'
-        f'{output}'
-    )
+    return f'''
+<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+{_instr}
+<|eot_id|>
+<|start_header_id|>user<|end_header_id|>
+{_examples[0]}
+<|eot_id|>
+<|start_header_id|>assistant<|end_header_id|>
+{_examples[1]}
+<|eot_id|>
+<|start_header_id|>user<|end_header_id|>
+{_examples[2]}
+<|eot_id|>
+<|start_header_id|>assistant<|end_header_id|>
+{_examples[3]}
+<|eot_id|>
+<|start_header_id|>user<|end_header_id|>
+{_examples[4]}
+<|eot_id|>
+<|start_header_id|>assistant<|end_header_id|>
+{_examples[5]}
+<|eot_id|>
+<|start_header_id|>user<|end_header_id|>
+{instruction}
+<|eot_id|>
+<|start_header_id|>assistant<|end_header_id|>
+{output}
+<|eot_id|><|end_of_text|>
+'''
