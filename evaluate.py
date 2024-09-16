1bb1f3cbac9fdfb4f33e3d5a11dc6de8a0a63474
greatericonrop <greateric@protonmail.com>
2024-09-16 11:30:35 -0400

    output raw output, for now

--- evaluate.py
+++ evaluate.py
@@ -64,7 +64,7 @@ def main():
             )
         s = generation_output.sequences[0]
         output = tokenizer.decode(s)
-        return output.split("### Response:")[1].strip()
+        return output.strip()
 
     """
     # testing code for readme
