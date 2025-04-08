13162216ea54b36ea1c3ccdf4f80b2614643b769
greatericontop <greateric@protonmail.com>
2025-04-07 21:44:37 -0400

    reduce size for manual grading

--- manualgrade.py
+++ manualgrade.py
@@ -6,11 +6,14 @@ import sys
 filename = sys.argv[1]
 save_file = 'manual_grade_logs.txt'
 
+print('[only doing 8 questions]')
+input('...')
+
 with open(filename, 'r') as f:
     responses = json.load(f)
 
 running_score = 0
-for i, data in enumerate(responses):
+for i, data in enumerate(responses[:8]):
     print('\033[H\033[2J\033[3J')
     print('----------------------------------------------------------------------------------------------------')
     print('----------------------------------------------------------------------------------------------------')
